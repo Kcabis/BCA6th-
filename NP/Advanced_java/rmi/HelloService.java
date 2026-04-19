6e3136a81be7bdb8b589d399ed66e68f8ce92961
Kcabis <165569849+Kcabis@users.noreply.github.com>
2026-04-19 13:53:16 +0545

    Delete NP directory

--- NP/Advanced_java/rmi/HelloService.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-
-public interface HelloService extends Remote {
-    // This method will be executed on the server but called by the client
-    String sayHello(String name) throws RemoteException;
-}
